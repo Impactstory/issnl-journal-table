b4160d177cdc2e100727ef41ad73a1b4b77201dc
Richard Orr <richard@impactstory.org>
2019-08-10 10:17:23 -0400

    replace journal metadata views with CTEs

--- 04-update-journal-titles.sql
+++ 04-update-journal-titles.sql
@@ -1,5 +1,50 @@
 -- set journal title and publisher from crossref or issn metadata
--- journal_*_titles view definitions are in this repo
+
+with
+
+crossref_info as (
+    select journal.issn_l,
+        (journal.api_raw_crossref->'message')->>'title' as title,
+        (journal.api_raw_crossref->'message')->>'publisher' as publisher
+    from journal
+),
+
+issn_org_info as (
+    select
+        z.issn_l,
+        min(z.main_title) as main_title,
+        min(z.name) as name,
+        coalesce(min(z.main_title), min(z.name)) AS title
+    from (
+        select
+            y.issn_l,
+            y.issns,
+            case
+                when jsonb_typeof(y.main_title) = 'array' then y.main_title->>0
+                else btrim(y.main_title::text, '"')
+            end as main_title,
+            case
+                when jsonb_typeof(y.name) = 'array' then y.name->>0
+                else btrim(y.name::text, '"')
+            end as name
+        from (
+            select
+                x.issn_l,
+                x.issns,
+                x.element->'mainTitle' as main_title,
+                x.element->'name' as name
+            from (
+                select
+                    journal.issn_l,
+                    journal.issns,
+                    jsonb_array_elements(journal.api_raw_issn->'@graph') as element
+                from journal
+            ) x
+            where (x.element->>'@id') = ('resource/ISSN/' || x.issn_l)
+        ) y
+    ) z
+    group by z.issn_l
+)
 
 update journal
 set
@@ -13,6 +58,7 @@ from (
         c.publisher as c_publisher
     from
         journal j
-        left join journal_crossref_titles c using (issn_l)
-        left join journal_issn_titles i using (issn_l)
-) x where journal.issn_l = x.issn_l;
+        left join crossref_info c using (issn_l)
+        left join issn_org_info i using (issn_l)
+) x
+where journal.issn_l = x.issn_l;
--- journal_crossref_titles.sql
+++
@@ -1,6 +0,0 @@
-create or replace view journal_crossref_titles as (
-SELECT journal.issn_l,
-    (journal.api_raw_crossref -> 'message'::text) ->> 'title'::text AS title,
-    (journal.api_raw_crossref -> 'message'::text) ->> 'publisher'::text AS publisher
-   FROM journal
-);
